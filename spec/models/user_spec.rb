16c378e21bfb339c7c872c5afc7f0dace278cd10
stevepm <steven.magelowitz@gmail.com>
2014-04-25 16:06:49 -0600

    tested that generate token works

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates a user with a token hash" do
+    user = User.new
+    user.save
+    user.generate_token!
+    expect(user.token.present?).to eq(true)
+  end
 end
